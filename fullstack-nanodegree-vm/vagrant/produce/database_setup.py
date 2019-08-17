1f3f46776294e5d4e9039469d97fbadea1af6743
FreemanWilliam <myhousemyrules69@yahoo.com>
2019-08-17 15:56:46 -0400

    Import Added
    Added the import for create_engine from sqlalchemy.

--- fullstack-nanodegree-vm/vagrant/produce/database_setup.py
+++ fullstack-nanodegree-vm/vagrant/produce/database_setup.py
@@ -3,3 +3,4 @@ import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
