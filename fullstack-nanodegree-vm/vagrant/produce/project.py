9dab6177a413dba0ba7a29591f24a60885723834
FreemanWilliam <myhousemyrules69@yahoo.com>
2019-08-18 15:51:23 -0400

    Query Added
    Added a query to the Produce Class to select the first item in the produce table.

--- fullstack-nanodegree-vm/vagrant/produce/project.py
+++ fullstack-nanodegree-vm/vagrant/produce/project.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
-app = Flask(__name__)
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Produce, ProduceItem
+
+app = Flask(__name__)
 
 engine = create_engine('sqlite:///producemenu.db')
 Base.metadata.bind = engine
@@ -15,7 +14,8 @@ session = DBSession()
 
 @app.route('/')
 @app.route('/produce/<int:produce_id>/')
-def Produce(produce_id):	
+def Produce(produce_id):
+	produce = session.query(Produce).filter_by(id=produce_id).one()
 	return 'This is the Produce Function'
 
 if __name__ == '__main__':
