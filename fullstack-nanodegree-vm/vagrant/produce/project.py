f71c1343871f9e5d14d29eda283d47b877a5c4ef
FreemanWilliam <myhousemyrules69@yahoo.com>
2019-08-18 15:29:18 -0400

    Line Deleted
    Deleted line Nineteen.

--- fullstack-nanodegree-vm/vagrant/produce/project.py
+++ fullstack-nanodegree-vm/vagrant/produce/project.py
@@ -15,8 +15,7 @@ session = DBSession()
 
 @app.route('/')
 @app.route('/produce/<int:produce_id>/')
-def Produce(produce_id):
-	produce = session.query(Produce).filter_by(id = produce_id).one()
+def Produce(produce_id):	
 	return 'This is the Produce Function'
 
 if __name__ == '__main__':
