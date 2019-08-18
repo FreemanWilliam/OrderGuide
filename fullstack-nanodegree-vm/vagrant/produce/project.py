22ed4e584376ab80a35fba77b9ddf2342c207d62
FreemanWilliam <myhousemyrules69@yahoo.com>
2019-08-18 15:26:15 -0400

    Query Added
    Added a query to the Produce Class to get the first item in the produce table.

--- fullstack-nanodegree-vm/vagrant/produce/project.py
+++ fullstack-nanodegree-vm/vagrant/produce/project.py
@@ -16,6 +16,7 @@ session = DBSession()
 @app.route('/')
 @app.route('/produce/<int:produce_id>/')
 def Produce(produce_id):
+	produce = session.query(Produce).filter_by(id = produce_id).one()
 	return 'This is the Produce Function'
 
 if __name__ == '__main__':
