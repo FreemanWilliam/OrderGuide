51f03035b88f1a6324b40dfda3bfe073c11771e9
FreemanWilliam <myhousemyrules69@yahoo.com>
2019-08-22 16:02:11 -0400

    Function Name Changed
    Changed the the fuction named Produce to getProduce.

--- fullstack-nanodegree-vm/vagrant/produce/project.py
+++ fullstack-nanodegree-vm/vagrant/produce/project.py
@@ -14,7 +14,7 @@ session = DBSession()
 
 @app.route('/')
 @app.route('/produce/<int:produce_id>/')
-def Produce(produce_id):
+def getProduce(produce_id):
 	produce = session.query(Produce).filter_by(id=produce_id).one()
 	return 'This is the Produce Function'
 
