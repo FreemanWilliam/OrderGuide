01fe12b550b378703496e9ebba9eef2a51e6af10
FreemanWilliam <myhousemyrules69@yahoo.com>
2019-08-25 16:29:49 -0400

    Route Added
    Added a route decorator to the newMenuItem function.

--- fullstack-nanodegree-vm/vagrant/produce/project.py
+++ fullstack-nanodegree-vm/vagrant/produce/project.py
@@ -18,7 +18,9 @@ def getProduce(produce_id):
 	produce = session.query(Produce).filter_by(id=produce_id).one()
 	items = session.query(ProduceItem).filter_by(produce_id=produce_id)
 	return render_template('menu.html',produce=produce, items=items)
+
 	
+@app.route('/produce/<int:produce_id>/new/')
 def newMenuItem(produce_id):
 
 if __name__ == '__main__':
