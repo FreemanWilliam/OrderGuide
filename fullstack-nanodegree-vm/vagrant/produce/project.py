dce59c8201af77a55c451ed399158f41bde65664
FreemanWilliam <myhousemyrules69@yahoo.com>
2019-08-29 16:17:10 -0400

    Passed the produce_id into the newItem Object in the newMenuItem function.                                                                                              File Edited: templates\project.py

--- fullstack-nanodegree-vm/vagrant/produce/project.py
+++ fullstack-nanodegree-vm/vagrant/produce/project.py
@@ -23,7 +23,7 @@ def getProduce(produce_id):
 @app.route('/produce/<int:produce_id>/new/', methods=['GET','POST'])
 def newMenuItem(produce_id):
 	if request.method == 'POST':
-		newItem = ProduceItem(name=request.form['name'])
+		newItem = ProduceItem(name=request.form['name'],produce_id = produce_id)
 	return "Test"
 
 if __name__ == '__main__':
