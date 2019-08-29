0fddbf10497fe3cbf8a3479f7fa2c7d928ccb3d3
FreemanWilliam <myhousemyrules69@yahoo.com>
2019-08-29 16:30:36 -0400

    Added the import for redirect.                                                                                                                                          File Edited: templates\project.py

--- fullstack-nanodegree-vm/vagrant/produce/project.py
+++ fullstack-nanodegree-vm/vagrant/produce/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Produce, ProduceItem
