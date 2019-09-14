12261be4ad1a369f3f30b083da97f1fa428c2e67
FreemanWilliam <myhousemyrules69@yahoo.com>
2019-09-14 16:44:37 -0400

    Line Break Added                                                                                                                                                        Added a line break on line 27.                                                                                                                                          Folder Edited: vagrant\produce

--- fullstack-nanodegree-vm/vagrant/produce/project.py
+++ fullstack-nanodegree-vm/vagrant/produce/project.py
@@ -24,7 +24,8 @@ def getProduce(produce_id):
 def newMenuItem(produce_id):
 	if request.method == 'POST':
 		newItem = ProduceItem(name=request.form['name'],id=request.form['id'], \
-		description=request.form['description'], price=request.form['price'],produce_id = produce_id)
+		description=request.form['description'], price=request.form['price'], \
+		,produce_id = produce_id)
 		session.add(newItem)
 		session.commit() 
 		return redirect(url_for('getProduce', produce_id=produce_id))		
